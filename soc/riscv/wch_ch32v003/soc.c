415c34ffb63dbee823cd7c5acdd52b3e8e713f9d
Michael Hope <mlhx@google.com>
2023-01-14 16:33:17 +0100

    soc: add wch_ch32v003 stubs

---
+++ soc/riscv/wch_ch32v003/soc.c
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021 Espressif Systems (Shanghai) Co., Ltd.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <soc.h>
+
+uint32_t sys_clock_elapsed(void)
+{
+  return 0;
+}
