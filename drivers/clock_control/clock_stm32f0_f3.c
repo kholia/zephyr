df191d713bef79582531a2b2219655a061081527
Dhiru Kholia <dhiru.kholia@gmail.com>
2024-11-16 15:29:42 +0530

    Add support for Puya PY32 chips
    Setup:
    
    ```
    pyocd pack find py32
    
    pyocd pack install PY32F072xB
    ```
    
    Build:
    
    ```
    west update
    
    west build -p always -b py32f030_startkit samples/basic/blinky
    
    west flash -r pyocd --target=py32f030x6
    ```
    
    Reference:
    
    https://github.com/IOsetting/py32f0-template/blob/main/Misc/SVD/py32f030xx.svd

--- drivers/clock_control/clock_stm32f0_f3.c
+++ drivers/clock_control/clock_stm32f0_f3.c
@@ -145,7 +145,7 @@ uint32_t get_pllout_frequency(void)
 void config_enable_default_clocks(void)
 {
 	/* Enable PWR clock, required to access BDCR and PWR_CR */
-	LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
+	LL_IOP_GRP1_EnableClock(LL_IOP_GRP1_PERIPH_GPIOA);
 
 #ifndef CONFIG_SOC_SERIES_STM32F3X
 #if defined(CONFIG_EXTI_STM32) || defined(CONFIG_USB_DC_STM32)
---
+++ soc/puya/py32/py32f0x/soc.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2017 RnDity Sp. z o.o.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file SoC configuration macros for the STM32F0 family processors.
+ *
+ * Based on reference manual:
+ *   STM32F030x4/x7/x8/xC,
+ *   STM32F070x7/xB advanced ARM ® -based MCUs
+ *
+ * Chapter 2.2: Memory organization
+ */
+
+
+#ifndef _STM32F0_SOC_H_
+#define _STM32F0_SOC_H_
+
+#ifndef _ASMLANGUAGE
+
+#include <py32f0xx.h>
+
+/* The STM32 HAL headers define these, but they conflict with the Zephyr can.h */
+#undef CAN_MODE_NORMAL
+#undef CAN_MODE_LOOPBACK
+
+#endif /* !_ASMLANGUAGE */
+
+#endif /* _STM32F0_SOC_H_ */
